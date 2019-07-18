b7b702aaa598bf707f466d8142708940499dca12
AngusParsonson <ap17691@my.bristol.ac.uk>
2019-07-18 13:25:18 +0100

    Basic start to the UI

--- IdeaProjects/Red_Herring/src/sample/Main.java
+++ IdeaProjects/Red_Herring/src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,32 +17,62 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Red Herring");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.getIcons().add(new Image("resources/logo.png"));
+
+        primaryStage.setScene(createMenu());
         primaryStage.show();
     }
 
-    public void createMenu() {
+    public Scene createMenu() {
         // create a menu
-        Menu m = new Menu("Project");
+        Menu project_menu = new Menu("Project");
+        Menu edit_menu = new Menu("Edit");
+        Menu help_menu = new Menu("Help");
+        Menu view_menu = new Menu("View");
 
         // create menuitems
-        MenuItem m1 = new MenuItem("New");
-        MenuItem m2 = new MenuItem("Open");
-        MenuItem m3 = new MenuItem("Exit");
+        MenuItem new_item  = new MenuItem("New");
+        MenuItem open_item = new MenuItem("Open");
+        MenuItem exit_item = new MenuItem("Exit");
+
+        MenuItem undo_item  = new MenuItem("Undo");
+        MenuItem redo_item  = new MenuItem("Redo");
+
+        MenuItem firefox_item = new MenuItem("Firefox");
+        MenuItem chrome_item  = new MenuItem("Chrome");
+        MenuItem edge_item  = new MenuItem("Edge");
+        MenuItem internet_explorer_item  = new MenuItem("Internet Explorer");
+
+        // add menu items to menu
+        project_menu.getItems().add(new_item);
+        project_menu.getItems().add(open_item);
+        project_menu.getItems().add(exit_item);
+
+        edit_menu.getItems().add(undo_item);
+        edit_menu.getItems().add(redo_item);
+
+        view_menu.getItems().add(firefox_item);
+        view_menu.getItems().add(chrome_item);
+        view_menu.getItems().add(edge_item);
+        view_menu.getItems().add(internet_explorer_item);
 
         // create a menubar
-        MenuBar mb = new MenuBar();
+        MenuBar menu_bar = new MenuBar();
 
         // add menu to menubar
-        mb.getMenus().add(m);
+        menu_bar.getMenus().add(project_menu);
+        menu_bar.getMenus().add(edit_menu);
+        menu_bar.getMenus().add(view_menu);
+        menu_bar.getMenus().add(help_menu);
 
         // create a VBox
-        VBox vb = new VBox(mb);
+        VBox vbox = new VBox(menu_bar);
 
         // create a scene
-        Scene sc = new Scene(vb, 500, 300);
-    }
+        Scene scene = new Scene(vbox, 1000, 800);
 
+        return scene;
+    }
 
     public static void main(String[] args) {
         launch(args);
