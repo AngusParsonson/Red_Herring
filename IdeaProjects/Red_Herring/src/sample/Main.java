af8d6974a5e54241166a3874f95fdde07bf09fbb
AngusParsonson <ap17691@my.bristol.ac.uk>
2019-07-22 14:43:19 +0100

    Refactored all code for MVC model using FXML

--- IdeaProjects/Red_Herring/src/sample/Main.java
+++ IdeaProjects/Red_Herring/src/sample/Main.java
@@ -3,22 +3,19 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.image.Image;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Red Herring");
-        primaryStage.getIcons().add(new Image("resources/logo.png"));
-
-        Menu_Methods menu = new Menu_Methods();
-
-        primaryStage.setScene(menu.create_menu(primaryStage));
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        primaryStage.setTitle("fxml_practice");
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
