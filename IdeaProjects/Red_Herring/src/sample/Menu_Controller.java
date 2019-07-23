9e103726c1df2f75ae56ebeda7f35af21a72b106
AngusParsonson <amfparsonson@gmail.com>
2019-07-23 20:09:38 +0100

    fixed bug where webview was overlapping menu when window was resized, cleaned up layout in general

--- IdeaProjects/Red_Herring/src/sample/Menu_Controller.java
+++ IdeaProjects/Red_Herring/src/sample/Menu_Controller.java
@@ -21,96 +21,95 @@ import java.util.ResourceBundle;
 
 public class Menu_Controller implements Initializable {
 
-        /** All functionality is wrapped in a separate function to allow for use in key shortcuts **/
-        @FXML
-        public GridPane gridPane;
+    /** All functionality is wrapped in a separate function to allow for use in key shortcuts **/
+    @FXML
+    public GridPane gridPane;
 
-        @FXML
-        private WebView webView;
+    @FXML
+    private WebView webView;
 
-        @FXML
-        private MenuBar menuBar;
+    @FXML
+    private MenuBar menuBar;
 
-        /** Handle key shortcut inputs **/
-        @FXML
-        private void handleKeyInput(final InputEvent event)
+    /** Handle key shortcut inputs **/
+    @FXML
+    private void handleKeyInput(final InputEvent event)
+    {
+        if (event instanceof KeyEvent)
         {
-            if (event instanceof KeyEvent)
+            final KeyEvent keyEvent = (KeyEvent) event;
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.A)
             {
-                final KeyEvent keyEvent = (KeyEvent) event;
-                if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.A)
-                {
-                    provideAboutFunctionality();
-                }
-                if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.N)
-                {
-                    provideNewFunctionality();
-                }
+                provideAboutFunctionality();
+            }
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.N)
+            {
+                provideNewFunctionality();
             }
         }
+    }
 
-        /** Logic for About menu option **/
-        @FXML
-        private void handleAboutAction(final ActionEvent event) {
-            provideAboutFunctionality();
+    /** Logic for About menu option **/
+    @FXML
+    private void handleAboutAction(final ActionEvent event) {
+        provideAboutFunctionality();
+    }
+
+    /** Logic for New menu option **/
+    @FXML
+    private void handleNewAction(final ActionEvent event) {
+        provideNewFunctionality();
+    }
+
+    /** Logic for Open menu option **/
+    @FXML
+    private void handleOpenAction(final ActionEvent e) {
+        provideOpenFunctionality(e);
+    }
+
+    private void provideAboutFunctionality() {
+        System.out.println("You clicked on About!");
+    }
+
+    private void provideNewFunctionality() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("url_window.fxml"));
+
+            Scene scene = new Scene(fxmlLoader.load(), 400, 50);
+            Stage stage = new Stage();
+            stage.setTitle("Enter url");
+
+            Url_Window_Controller controller = fxmlLoader.getController();
+            controller.setMenuController(this);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Failed to create new window");
         }
+    }
 
-        /** Logic for New menu option **/
-        @FXML
-        private void handleNewAction(final ActionEvent event) {
-            provideNewFunctionality();
+    private void provideOpenFunctionality(ActionEvent e) {
+        FileChooser file_chooser = new FileChooser();
+
+        File selected_file = file_chooser.showOpenDialog(menuBar.getScene().getWindow());
+        String url = null;
+        try {
+            url = selected_file.toURI().toURL().toString();
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
         }
+        open_browser_with_url(url);
 
-        /** Logic for Open menu option **/
-        @FXML
-        private void handleOpenAction(final ActionEvent e) {
-            provideOpenFunctionality(e);
-        }
+    }
 
-        private void provideAboutFunctionality() {
-            System.out.println("You clicked on About!");
-        }
+    @Override
+    public void initialize(java.net.URL arg0, ResourceBundle arg1) {
+        menuBar.setFocusTraversable(true);
+    }
 
-        private void provideNewFunctionality() {
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("url_window.fxml"));
-
-                Scene scene = new Scene(fxmlLoader.load(), 400, 50);
-                Stage stage = new Stage();
-                stage.setTitle("Enter url");
-
-                Url_Window_Controller controller = fxmlLoader.getController();
-                controller.setMenuController(this);
-
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                System.out.println("Failed to create new window");
-            }
-        }
-
-        private void provideOpenFunctionality(ActionEvent e) {
-            FileChooser file_chooser = new FileChooser();
-
-            File selected_file = file_chooser.showOpenDialog(menuBar.getScene().getWindow());
-            String url = null;
-            try {
-                url = selected_file.toURI().toURL().toString();
-            } catch (MalformedURLException ex) {
-                ex.printStackTrace();
-            }
-            open_browser_with_url(url);
-
-        }
-
-        @Override
-        public void initialize(java.net.URL arg0, ResourceBundle arg1) {
-            menuBar.setFocusTraversable(true);
-        }
-
-        public void open_browser_with_url(String url) {
-            webView.getEngine().load(url);
-        }
+    public void open_browser_with_url(String url) {
+        webView.getEngine().load(url);
+    }
 }
-
